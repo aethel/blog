ff4a896163c261a32f1c38fc98e3ad9696c3a6b1
aethel <aethelbrihte@gmail.com>
2017-03-28 20:42:36 +0100

    init

---
+++ app/app.module.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular.module('blog', [
+        'ngRoute',
+        'blog.core',
+        'blog.widgets',
+        'blog.layout',
+        'blog.bloglist'
+    ])
+
+})();
